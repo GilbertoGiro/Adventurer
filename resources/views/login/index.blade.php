2d79f92b5467e9afad62a6cc9fb80ada15e77352
Gilberto Giro Resende <gilberto.giro.resende@gmail.com>
2018-03-21 00:53:47 -0300

    Desenvolvimento do Sistema de Eventos - Adventurer - Módulo Web - Lay…

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,8 +1,218 @@
 @extends('layouts.app')
 
 @section('content')
+    <h2 class="roboto header m-t-sm">Quais as funcionalidades da aplicação?</h2>
+
+    <div class="p-sm text-center m-b-lg">
+        <div class="card block" style="width:24%;">
+            <div class="card-header text-center background-red">
+                <h3 class="m-t-sm white">Inscrição Facilitada</h3>
+            </div>
+            <div class="card-body p-sm m-t-sm">
+                <div class="small-circular-image">
+                    <img src="{{ asset('img/event/png/046-ticket.png') }}" width="70px">
+                </div>
+                
+                <div class="block p-sm">
+
+                </div>
+            </div>
+        </div>
+
+        <div class="card block m-l-sm" style="width:24%;">
+            <div class="card-header text-center background-gold">
+                <h3 class="m-t-sm white">Sugestão de Temas</h3>
+            </div>
+            <div class="card-body p-sm m-t-sm">
+                <div class="small-circular-image">
+                    <img src="{{ asset('img/event/png/046-ticket.png') }}" width="70px">
+                </div>
+
+                <div class="block p-sm">
+
+                </div>
+            </div>
+        </div>
+
+        <div class="card block m-l-sm" style="width:24%;">
+            <div class="card-header text-center background-bronze">
+                <h3 class="m-t-sm white">Relatórios Semanais</h3>
+            </div>
+            <div class="card-body p-sm m-t-sm">
+                <div class="small-circular-image">
+                    <img src="{{ asset('img/event/png/018-list.png') }}" width="70px">
+                </div>
+
+                <div class="block p-sm">
+
+                </div>
+            </div>
+        </div>
+
+        <div class="card block m-l-sm" style="width:24%;">
+            <div class="card-header text-center background-goldilocks">
+                <h3 class="m-t-sm white">Pontos de Participação</h3>
+            </div>
+            <div class="card-body p-sm m-t-sm">
+                <div class="small-circular-image">
+                    <img src="{{ asset('img/event/png/007-present.png') }}" width="70px">
+                </div>
+
+                <div class="block p-sm">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
     <h2 class="roboto header m-t-sm">Efetuar Login/Cadastro</h2>
 
+    <div class="p-sm text-center">
+        <div class="card text-center block m-r-md" style="width:32%;">
+            <div class="card-header background-weak-blue">
+                <h3 class="m-t-sm white">Já possui conta? Efetue o login aqui!</h3>
+            </div>
+            <div class="card-body p-sm m-t-sm">
+                <div class="small-circular-image">
+                    <img src="{{ asset('img/event/png/016-graduation-hat.png') }}" width="65px">
+                </div>
+
+                <form class="block p-md" method="post" action="">
+                    <div class="form-group">
+                        <input type="email" name="email" class="form-input" id="email" placeholder="E-mail">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="password" name="senha" class="form-input" id="senha" placeholder="Senha">
+                    </div>
+
+                    <div class="form-group m-t-sm">
+                        <button type="submit" class="button button-success form-input">Efetuar o Login <i class="fa fa-sign-in-alt"></i></button>
+                        <button type="submit" class="button button-warning form-input">Esqueci minha Senha <i class="fa fa-question-circle"></i></button>
+                    </div>
+                </form>
+            </div>
+            <div class="card-footer background-weak-blue">
+                <span class="inline-block">
+                    <i class="fab fa-facebook-square medium white"></i>
+                </span>
+                <span class="inline-block m-l-sm">
+                    <i class="fab fa-twitter-square medium white"></i>
+                </span>
+                <span class="inline-block m-l-sm">
+                    <i class="fa fa-envelope-square medium white"></i>
+                </span>
+                <span class="inline-block m-l-sm">
+                    <i class="fab fa-linkedin medium white"></i>
+                </span>
+            </div>
+        </div>
+        <div class="card text-center block m-r-md" style="width:32%;">
+            <div class="card-header background-weak-blue">
+                <h3 class="m-t-sm white">Não possui uma conta? Crie agora mesmo!</h3>
+            </div>
+            <div class="card-body p-sm m-t-sm">
+                <div class="small-circular-image">
+                    <img src="{{ asset('img/event/png/035-barrier.png') }}" width="65px">
+                </div>
+
+                <form class="block p-md" method="post" action="">
+                    <div class="form-group">
+                        <input type="text" name="nome" class="form-input" id="nome" placeholder="Nome">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="email" name="email" class="form-input" id="email" placeholder="E-mail">
+                    </div>
+
+                    <div class="form-group">
+                        <select name="curso" class="form-input" id="curso">
+                            <option value="">Curso</option>
+                            <option value="ads">Análise e Desenvolvimento de Sistemas</option>
+                            <option value="gti">Gestão da Tecnologia da Informação</option>
+                            <option value="gam">Gestão Ambiental</option>
+                            <option value="eve">Eventos</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="password" name="senha" class="form-input" id="senha" placeholder="Senha">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="password" name="repeticao" class="form-input" id="repeticao" placeholder="Repita a Senha">
+                    </div>
+
+                    <div class="form-group m-t-sm">
+                        <button type="submit" class="button button-success form-input">Cadastrar Conta <i class="fa fa-sign-in-alt"></i></button>
+                    </div>
+                </form>
+            </div>
+            <div class="card-footer background-weak-blue">
+                <span class="inline-block">
+                    <i class="fab fa-facebook-square medium white"></i>
+                </span>
+                <span class="inline-block m-l-sm">
+                    <i class="fab fa-twitter-square medium white"></i>
+                </span>
+                <span class="inline-block m-l-sm">
+                    <i class="fa fa-envelope-square medium white"></i>
+                </span>
+                <span class="inline-block m-l-sm">
+                    <i class="fab fa-linkedin medium white"></i>
+                </span>
+            </div>
+        </div>
+
+        <div class="card text-center block" style="width:32%;">
+            <div class="card-header background-weak-blue">
+                <h3 class="m-t-sm white">Deseja receber notificações de eventos?</h3>
+            </div>
+            <div class="card-body p-sm m-t-sm">
+                <div class="small-circular-image">
+                    <img src="{{ asset('img/event/png/009-invitation.png') }}" width="65px">
+                </div>
+
+                <form class="block p-md" method="post" action="">
+                    <div class="form-group">
+                        <input type="text" name="nome" class="form-input" id="nome" placeholder="Nome">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="email" name="email" class="form-input" id="email" placeholder="E-mail">
+                    </div>
+
+                    <div class="form-group">
+                        <select name="curso" class="form-input" id="curso">
+                            <option value="">Curso</option>
+                            <option value="ads">Análise e Desenvolvimento de Sistemas</option>
+                            <option value="gti">Gestão da Tecnologia da Informação</option>
+                            <option value="gam">Gestão Ambiental</option>
+                            <option value="eve">Eventos</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group m-t-sm">
+                        <button type="submit" class="button button-success form-input">Habilitar E-mails <i class="fa fa-envelope"></i></button>
+                    </div>
+                </form>
+            </div>
+            <div class="card-footer background-weak-blue">
+                <span class="inline-block">
+                    <i class="fab fa-facebook-square medium white"></i>
+                </span>
+                <span class="inline-block m-l-sm">
+                    <i class="fab fa-twitter-square medium white"></i>
+                </span>
+                <span class="inline-block m-l-sm">
+                    <i class="fa fa-envelope-square medium white"></i>
+                </span>
+                <span class="inline-block m-l-sm">
+                    <i class="fab fa-linkedin medium white"></i>
+                </span>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('scripts')
